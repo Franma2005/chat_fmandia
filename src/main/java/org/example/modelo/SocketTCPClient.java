d919f471032a54650d251ddc28d202eff25b8562
fmandia <fmandia1201@gmail.com>
2025-01-30 22:11:19 +0100

    Vamos a cambiar el programa para que envie un json. Si tan solo uso texto plano el usuario podría mediante sus mensajes realizar operaciones que no quiero que realice

--- src/main/java/org/example/modelo/SocketTCPClient.java
+++ src/main/java/org/example/modelo/SocketTCPClient.java
@@ -19,9 +19,7 @@ public class SocketTCPClient {
 
     private void startSocket(String ipServer, int port) {
         try {
-            System.out.println("(Client) Starting socket");
             socket = new Socket(ipServer, port);
-            System.out.println("(Client) Socket is start");
         } catch (IOException exception) {
             System.out.println("Error al inicializar el socket del cliente");
         }
@@ -30,29 +28,23 @@ public class SocketTCPClient {
 
     public void startBytesChannels() {
         try {
-            System.out.println("(Client) Opening the channels of communication");
             out = socket.getOutputStream();
             in = socket.getInputStream();
-            System.out.println("(Client) The channels of communication have been opened");
         } catch (IOException exception) {
             System.out.println("Exception open the bytes channels of the socket");
         }
     }
 
     public void startTextChannels() {
-        System.out.println("(Client) Opening the text channels of communication");
         br = new BufferedReader(new InputStreamReader(in));
         pw = new PrintWriter(out, true);
-        System.out.println("(Client) The text channels of communication have been openned");
     }
 
     public void stopBytesChannels() {
         try {
-            System.out.println("(Client) Closing the channels of communication");
             in.close();
             out.close();
             socket.close();
-            System.out.println("(Client) The channels of communication have been opened");
         } catch (IOException exception) {
             System.out.println("Exception close the bytes channels of the socket");
         }
@@ -60,10 +52,8 @@ public class SocketTCPClient {
 
     public void stopTextChannels() {
         try {
-            System.out.println("(Client) Closing the text channels of communicantion");
             br.close();
             pw.close();
-            System.out.println("(Client) The text channels of communication have been closed");
         } catch (IOException exception) {
             System.out.println("Exception close the text channels of the socket");
         }
@@ -80,10 +70,4 @@ public class SocketTCPClient {
     public PrintWriter getPw() {
         return pw;
     }
-
-    /*public void closeServerChannels() {
-        System.out.println("Disconnect" + );
-        getPw().println();
-        getPw().flush();
-    }*/
 }
